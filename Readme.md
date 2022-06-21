09a61e2bcd3acf9bc77527a6ac99b87178fe5021
carla-day <carladay44@gmail.com>
2022-06-21 12:48:35 -0400

    working on read me

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# Project Title
+
+Service Now Data Visualization with Python
+
+## Description
+
+Using sample API data from Service Now (Rest API Explorer), I created two bar graphs to analyze how the service team is handling incidents, what category does the service team need to work on, and 
+what categories seem to be doing well. 
+
+### Executing program
+To start, you must have python version 3 installed and pip 
+--Use this link to install pip https://www.geeksforgeeks.org/how-to-install-pip-on-windows/
+
+Once pip is installed we must install the libraries and framework we will use
+* pip install flask
+* pip install requests
+* pip install pandas
+    We do not need matplotlib as we will be using Chart.js to graph.
+
+## Authors
+Carla Day
+[![Carla’s github stats](https://github-readme-stats.vercel.app/api?username=carla-day)](https://github.com/carla-day) 
+
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [awesome-readme](https://github.com/matiassingers/awesome-readme)
+* [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
+* [dbader](https://github.com/dbader/readme-template)
+* [zenorocha](https://gist.github.com/zenorocha/4526327)
+* [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)
---
+++ app.py
@@ -0,0 +1,121 @@
+from flask import Flask, render_template
+from numpy import True_
+import pandas as pd
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("home.html", title="Home")
+
+
+@app.route('/chart1')
+def chart1():
+    url = 'https://dev103544.service-now.com/api/now/table/incident?sysparm_display_value=all&sysparm_fields=sys_class_name%2Ccategory%2Cseverity%2Ccorrelation_display%2Cclose_code'
+
+# Eg. User name="admin", Password="admin" for this code sample.
+    user = 'admin'
+    pwd = 'Zs+IQVw7*ai8'
+
+# Set proper headers
+    headers = {"Content-Type":"application/json","Accept":"application/json"}
+
+# Do the HTTP request
+    response = requests.get(url, auth=(user, pwd), headers=headers )
+
+# Check for HTTP codes other than 200
+    if response.status_code != 200: 
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+        exit()
+
+# Decode the JSON response into a dictionary and use the data
+
+    data = response.json()
+    severity = []
+    category=[]
+    index1 = 0
+    #This graph shows the categories that have severe incidents
+    
+    high_cat= []
+    high_value = []
+    low_cat=[]
+  
+    for severe in data['result']:
+        severity.append(data['result'][index1]['severity']['value'])
+        category.append(data['result'][index1]['category']['value'].upper())
+        if severity[index1] == '1':
+            high_cat.append(category[index1])
+            high_value.append(severity[index1])
+        else:
+            low_cat.append(category[index1].upper())
+        index1 +=1
+    cat= []
+    values = []
+    for i in high_cat: 
+        if i not in cat: 
+            cat.append(i)
+    for i in cat:
+        count = pd.Series(high_cat).value_counts()
+        values.append(count[i])
+
+    return render_template("index.html", title="Graph 1", cat=cat, values=values)
+
+
+@app.route('/chart2')
+def chart2():
+    url = 'https://dev103544.service-now.com/api/now/table/incident?sysparm_display_value=all&sysparm_fields=sys_class_name%2Ccategory%2Cseverity%2Ccorrelation_display%2Cclose_code'
+
+# Eg. User name="admin", Password="admin" for this code sample.
+    user = 'admin'
+    pwd = 'Zs+IQVw7*ai8'
+
+# Set proper headers
+    headers = {"Content-Type":"application/json","Accept":"application/json"}
+
+# Do the HTTP request
+    response = requests.get(url, auth=(user, pwd), headers=headers )
+
+# Check for HTTP codes other than 200
+    if response.status_code != 200: 
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+        exit()
+
+# Decode the JSON response into a dictionary and use the data
+
+    data = response.json()
+    severity = []
+    category=[]
+    index1 = 0
+    #This graph shows the categories that have severe incidents
+    
+    high_cat= []
+    high_value = []
+    low_cat=[]
+  
+    for severe in data['result']:
+        severity.append(data['result'][index1]['severity']['value'])
+        category.append(data['result'][index1]['category']['value'].upper())
+        if severity[index1] == '1':
+            high_cat.append(category[index1])
+            high_value.append(severity[index1])
+        else:
+            low_cat.append(category[index1].upper())
+        index1 +=1
+    cat= []
+    values = []
+    #get categories without repetitions
+    for i in low_cat: 
+        if i not in cat: 
+            cat.append(i)
+            #get values
+    for i in cat:
+        count = pd.Series(low_cat).value_counts()
+        values.append(count[i])
+        #no empty categories
+    for i in cat:
+        if i == '':
+            j = cat.index(i)
+            cat[j] = 'UNKNOWN'
+
+    return render_template("chart2.html", title="Graph 2", cat=cat, values=values)
