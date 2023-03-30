b1eb76aa3ec5ba4ade5c829ad888f89528e16d75
carla-day <carladay44@gmail.com>
2023-03-30 10:59:43 -0400

    updated app

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
 import pandas as pd
 import requests
-from environ import Env
-
-env = Env()
-env.read_env()  # read .env file, if it exists
-# required variables
+import os 
 
 app = Flask(__name__)
 
@@ -19,52 +15,53 @@ def chart1():
     url = 'https://dev157572.service-now.com/api/now/table/incident?sysparm_display_value=all&sysparm_fields=sys_class_name%2Ccategory%2Cseverity%2Ccorrelation_display%2Cclose_code'
 
 # Eg. User name="admin", Password="admin" for this code sample.
-    user = env("USER")
-    pwd = env("PASSWORD")
+    user = os.getenv("USER")
+    pwd = os.getenv("PASSWORD")
 
 # Set proper headers
     headers = {"Content-Type":"application/json","Accept":"application/json"}
 
 # Do the HTTP request
     response = requests.get(url, auth=(user, pwd), headers=headers )
-
+    #print(response.status_code)
 # Check for HTTP codes other than 200
-    if response.status_code != 200: 
-        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
-        return render_template("hibernate.html")
-        exit()
+    try:
 
 # Decode the JSON response into a dictionary and use the data
-
-    data = response.json()
-    severity = []
-    category=[]
-    index1 = 0
+        data = response.json()
+    
+        severity = []
+        category=[]
+        index1 = 0
     #This graph shows the categories that have severe incidents
     
-    high_cat= []
-    high_value = []
-    low_cat=[]
+        high_cat= []
+        high_value = []
+        low_cat=[]
   
-    for severe in data['result']:
-        severity.append(data['result'][index1]['severity']['value'])
-        category.append(data['result'][index1]['category']['value'].upper())
-        if severity[index1] == '1':
-            high_cat.append(category[index1])
-            high_value.append(severity[index1])
-        else:
-            low_cat.append(category[index1].upper())
-        index1 +=1
-    cat= []
-    values = []
-    for i in high_cat: 
-        if i not in cat: 
-            cat.append(i)
-    for i in cat:
-        count = pd.Series(high_cat).value_counts()
-        values.append(count[i])
+        for severe in data['result']:
+            severity.append(data['result'][index1]['severity']['value'])
+            category.append(data['result'][index1]['category']['value'].upper())
+            if severity[index1] == '1':
+                high_cat.append(category[index1])
+                high_value.append(severity[index1])
+            else:
+                low_cat.append(category[index1].upper())
+            index1 +=1
+        cat= []
+        values = []
+        for i in high_cat: 
+            if i not in cat: 
+                cat.append(i)
+        for i in cat:
+            count = pd.Series(high_cat).value_counts()
+            values.append(count[i])
 
-    return render_template("index.html", title="Graph 1", cat=cat, values=values)
+        return render_template("index.html", title="Graph 1", cat=cat, values=values)
+
+    except:
+        return render_template("hibernate.html")
+
 
 
 @app.route('/chart2')
@@ -72,9 +69,9 @@ def chart2():
     url = 'https://dev157572.service-now.com/api/now/table/incident?sysparm_display_value=all&sysparm_fields=sys_class_name%2Ccategory%2Cseverity%2Ccorrelation_display%2Cclose_code'
 
 # Eg. User name="admin", Password="admin" for this code sample.
-    user = env("USER")
-    pwd = env("PASSWORD")
-
+    user = os.environ.get("USER")
+    pwd = os.environ.get("PASSWORD")
+    print(user," ", pwd)
 # Set proper headers
     headers = {"Content-Type":"application/json","Accept":"application/json"}
 
@@ -82,46 +79,49 @@ def chart2():
     response = requests.get(url, auth=(user, pwd), headers=headers )
 
 # Check for HTTP codes other than 200
-    if response.status_code != 200: 
-        #if the response is not 200 return html to advise user the instance is hibernating
-       return render_template("hibernate.html")
-       exit()
+   
 
 # Decode the JSON response into a dictionary and use the data
+    try:
 
-    data = response.json()
-    severity = []
-    category=[]
-    index1 = 0
+        data = response.json()
+        print(data)
+        severity = []
+        category=[]
+        index1 = 0
     #This graph shows the categories that have severe incidents
     
-    high_cat= []
-    high_value = []
-    low_cat=[]
+        high_cat= []
+        high_value = []
+        low_cat=[]
   
-    for severe in data['result']:
-        severity.append(data['result'][index1]['severity']['value'])
-        category.append(data['result'][index1]['category']['value'].upper())
-        if severity[index1] == '1':
-            high_cat.append(category[index1])
-            high_value.append(severity[index1])
-        else:
-            low_cat.append(category[index1].upper())
-        index1 +=1
-    cat= []
-    values = []
+        for severe in data['result']:
+            severity.append(data['result'][index1]['severity']['value'])
+            category.append(data['result'][index1]['category']['value'].upper())
+            if severity[index1] == '1':
+                high_cat.append(category[index1])
+                high_value.append(severity[index1])
+            else:
+                low_cat.append(category[index1].upper())
+            index1 +=1
+        cat= []
+        values = []
     #get categories without repetitions
-    for i in low_cat: 
-        if i not in cat: 
-            cat.append(i)
+        for i in low_cat: 
+            if i not in cat: 
+                cat.append(i)
             #get values
-    for i in cat:
-        count = pd.Series(low_cat).value_counts()
-        values.append(count[i])
+        for i in cat:
+            count = pd.Series(low_cat).value_counts()
+            values.append(count[i])
         #no empty categories
-    for i in cat:
-        if i == '':
-            j = cat.index(i)
-            cat[j] = 'UNKNOWN'
+        for i in cat:
+            if i == '':
+                j = cat.index(i)
+                cat[j] = 'UNKNOWN'
 
-    return render_template("chart2.html", title="Graph 2", cat=cat, values=values)
+        return render_template("chart2.html", title="Graph 2", cat=cat, values=values)
+
+    except:
+
+                return render_template("hibernate.html")
